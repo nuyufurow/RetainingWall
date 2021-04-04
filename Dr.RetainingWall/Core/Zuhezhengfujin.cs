4b2f81b0973ac9f99aa7db06eeca4ab8ce424b71
liukk <liukk@glodon.com>
2021-04-04 18:13:40 +0800

    0404-2

--- Dr.RetainingWall/Core/Zuhezhengfujin.cs
+++ Dr.RetainingWall/Core/Zuhezhengfujin.cs
@@ -431,11 +431,13 @@ namespace Dr.RetainingWall
 
             double[] amin = A[0];
             double[] amax = A[1];
+            double pm = M[0];
             bool isExchange = false;
             if (A[0][0] >= A[1][0])
             {
                 amin = A[1];
                 amax = A[0];
+                pm = M[2];
                 isExchange = true;
             }
 
@@ -454,33 +456,64 @@ namespace Dr.RetainingWall
                             double area1 = Math.PI * Math.Pow(d[i], 2) / 4 * 1000 / 150 + Math.PI * Math.Pow(d[j], 2) / 4 * 1000 / 150;
                             double area2 = Math.PI * Math.Pow(d[i], 2) / 4 * 1000 / 150 + Math.PI * Math.Pow(d[j], 2) / 4 * 1000 / 300;
 
-                            if((area < area2 && area2 - area < delta)|| (area > area2))
+                            if (area < area2 && area2 - area < delta)
                             {
                                 delta = area2 - area;
                                 amin[2] = d[j];
                                 amin[3] = 300;
                                 amin[4] = area2;
                                 double[] dd = { amin[0], Math.Floor(1000 / amin[1]), amin[2], Math.Floor(1000 / amin[3]) };
-                                double w1 = Zuhezhengfujin.liefeng1(M[2], cs, dd, amin[4], h[0], C, rg);
-                                double w2 = Zuhezhengfujin.liefeng1(M[2], cs, dd, amin[4], h[1], C, rg);
+                                double w1 = Zuhezhengfujin.liefeng1(pm, cs, dd, amin[4], h[0], C, rg);
+                                double w2 = Zuhezhengfujin.liefeng1(pm, cs, dd, amin[4], h[1], C, rg);
                                 double w = Math.Max(w1, w2);
                                 amin[5] = w;
                             }
-                            else if (area < area1 && area1 - area < delta)
+                            else if (area > area2)
+                            {
+                                double[] dd = { amin[0], Math.Floor(1000 / amin[1]), d[j], Math.Floor(1000 / 300.0) };
+                                double w1 = Zuhezhengfujin.liefeng1(pm, cs, dd, area2, h[0], C, rg);
+                                double w2 = Zuhezhengfujin.liefeng1(pm, cs, dd, area2, h[1], C, rg);
+                                double w = Math.Max(w1, w2);
+                                if (w < 0.2)
+                                {
+                                    delta = area2 - area;
+                                    amin[2] = d[j];
+                                    amin[3] = 300;
+                                    amin[4] = area2;
+                                    amin[5] = w;
+                                }
+                            }
+
+
+                            if (area < area1 && area1 - area < delta)
                             {
                                 delta = area1 - area;
                                 amin[2] = d[j];
                                 amin[3] = 150;
                                 amin[4] = area1;
                                 double[] dd = { amin[0], Math.Floor(1000 / amin[1]), amin[2], Math.Floor(1000 / amin[3]) };
-                                double w1 = Zuhezhengfujin.liefeng1(M[2], cs, dd, amin[4], h[0], C, rg);
-                                double w2 = Zuhezhengfujin.liefeng1(M[2], cs, dd, amin[4], h[1], C, rg);
+                                double w1 = Zuhezhengfujin.liefeng1(pm, cs, dd, amin[4], h[0], C, rg);
+                                double w2 = Zuhezhengfujin.liefeng1(pm, cs, dd, amin[4], h[1], C, rg);
                                 double w = Math.Max(w1, w2);
                                 amin[5] = w;
                             }
+                            else if(area > area1)
+                            {
+                                double[] dd = { amin[0], Math.Floor(1000 / amin[1]), d[j], Math.Floor(1000 / 150.0) };
+                                double w1 = Zuhezhengfujin.liefeng1(pm, cs, dd, area1, h[0], C, rg);
+                                double w2 = Zuhezhengfujin.liefeng1(pm, cs, dd, area1, h[1], C, rg);
+                                double w = Math.Max(w1, w2);
+                                if (w < 0.2)
+                                {
+                                    delta = area1 - area;
+                                    amin[2] = d[j];
+                                    amin[3] = 300;
+                                    amin[4] = area1;
+                                    amin[5] = w;
+                                }
+                            }
                         }
                     }
-
                     break;
                 }
             }
