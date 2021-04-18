7f9c33bcd283983c3eb8d8d2ca110b83f614367d
nuyufurow <nuyufurow@outlook.com>
2021-04-18 20:13:01 +0800

    0418-1

--- Dr.RetainingWall/Core/Chengben.cs
+++ Dr.RetainingWall/Core/Chengben.cs
@@ -140,60 +140,12 @@ namespace Dr.RetainingWall
                 double Qff150 = (1000 / Ass[0][3]) * (Math.Pow(Ass[0][2] / 1000, 2) * Math.PI / 4) * (Lff / 1000) * 7.85 * Qg;//Qff为附加顶筋间距150费用
                 double Qft200 = (1000 / Ass[2][1]) * (Math.Pow(Ass[2][0] / 1000, 2) * Math.PI / 4) * (Lft200 / 1000) * 7.85 * Qg;//Qft为通长顶筋间距200费用
                 double Qff200 = (1000 / Ass[2][3]) * (Math.Pow(Ass[2][2] / 1000, 2) * Math.PI / 4) * (Lff / 1000) * 7.85 * Qg;//Qff为附加顶筋间距200费用
-                double Qz = (1000 / Ass[4][1]) * (Math.Pow(Ass[4][0] / 1000, 2) * Math.PI / 4) * (Lz / 1000) * 7.85 * Qg;//Qz为底筋费用
+                double Qz150 = (1000 / Ass[4][1]) * (Math.Pow(Ass[4][0] / 1000, 2) * Math.PI / 4) * (Lz / 1000) * 7.85 * Qg;//Qz为底筋费用
+                double Qz200 = (1000 / Ass[5][1]) * (Math.Pow(Ass[5][0] / 1000, 2) * Math.PI / 4) * (Lz / 1000) * 7.85 * Qg;//Qz为底筋费用
                 double Qt = (1 * h[0] / 1000) * (H[0] / 1000) * Qh;//Qt为混凝土费用
                 double QF150 = Qft150 + Qff150;// QF150为间距150所有钢筋的费用
                 double QF200 = Qft200 + Qff200;// QF200为间距200所有钢筋的费用
-                double[] Q = new double[] { QF150, QF200, Qz, Qs, Qt, QF150 + Qz + Qs + Qt, QF200 + Qz + Qs + Qt };
-                return Q;
-            }
-            else if (n == 2)
-            {
-                double Lff1 = Math.Round((H[0] - s[0]) / 3 / 10, 0) * 10 + 10;//1层层高净高三分之一
-                double Lff2 = Math.Round((H[1] - s[1]) / 3 / 10, 0) * 10 + 10;//2层层高净高三分之一
-                double LffB = 2 * Math.Max(Lff1, Lff2) + s[0];//B点附加筋总长度
-                double LffA = (H[0] - s[0]) / 3;//A点附加筋总长度
-
-                double Lft1150 = H[0] + LffB + Math.Max(35 * Math.Max(Ass[0][0], Ass[2][0]), 500);//Lft为1层150通长顶筋的长度
-                double Lft1200 = H[0] + LffB + Math.Max(35 * Math.Max(Ass[3][0], Ass[5][0]), 500);//Lft为1层150通长顶筋的长度
-                double Lft2150 = H[1] - Math.Max(Lff1, Lff2) - s[1] - Math.Max(35 * Math.Max(Ass[0][0], Ass[2][0]), 500) + maoguchangdu(CC, F, Ass[2][0]);//Lft为2层间距150通长顶筋的长度
-                double Lft2200 = H[1] - Math.Max(Lff1, Lff2) - s[1] - Math.Max(35 * Math.Max(Ass[3][0], Ass[5][0]), 500) + maoguchangdu(CC, F, Ass[5][0]);//Lft为2层间距200通长顶筋的长度
-                double Lz1, Lz2;
-                if (h[0] == h[1])//若1、2层墙厚相等，则用焊接或分别锚固
-                {
-                    if (Ass[6][1] == Ass[7][1])//一层和二层的底筋模数若相等，则采用电焊
-                    {
-                        Lz1 = H[0] + 500 + Math.Max(35 * Math.Max(Ass[6][0], Ass[7][0]), 500);//Lz为1层底筋的长度
-                        Lz2 = H[1] - 500 - Math.Max(35 * Math.Max(Ass[6][0], Ass[7][0]), 500) - s[1] + maoguchangdu(CC, F, Ass[7][0]);//Lz为2层底筋的长度
-                    }
-                    else//一层和二层的底筋模数不同，则分别锚固
-                    {
-                        double laE1 = maoguchangduE(Z, CC, F, Ass[6][0]);
-                        double laE2 = maoguchangduE(Z, CC, F, Ass[7][0]);
-                        Lz1 = H[0] - s[0] + 1.2 * laE1;            //一层底筋长度，忽略基础中的长度
-                        double laz = maoguchangdu(CC, F, Ass[7][0]);
-                        Lz2 = H[1] - s[1] + laz + 1.2 * laE2;      //二层底筋长度
-                    }
-                }
-                else//1、2层墙厚不相等，采用分别锚固
-                {
-                    Lz1 = H[0] - cs + 12 * Ass[6][0];
-                    double laE2 = maoguchangduE(Z, CC, F, Ass[7][0]);
-                    Lz2 = H[1] - s[1] + maoguchangdu(CC, F, Ass[7][0]) + 1.2 * laE2;
-                }
-
-                double Qft150 = ((1000 / Ass[0][1]) * (Math.Pow((Ass[0][0] / 1000), 2) * Math.PI / 4) * (Lft1150 / 1000) + (1000 / Ass[2][1]) * (Math.Pow((Ass[2][0] / 1000), 2) * Math.PI / 4) * (Lft2150 / 1000)) * 7.85 * Qg;  //Qft为通长顶筋间距150费用
-                double Qff150 = ((1000 / Ass[0][3]) * (Math.Pow((Ass[0][2] / 1000), 2) * Math.PI / 4) * (LffA / 1000) + (1000 / Ass[1][3]) * (Math.Pow((Ass[1][2] / 1000), 2) * Math.PI / 4) * (LffB / 1000)) * 7.85 * Qg;        //Qff为附加顶筋间距150费用
-                double Qft200 = ((1000 / Ass[3][1]) * (Math.Pow((Ass[3][0] / 1000), 2) * Math.PI / 4) * (Lft1200 / 1000) + (1000 / Ass[5][1]) * (Math.Pow((Ass[5][0] / 1000), 2) * Math.PI / 4) * (Lft2200 / 1000)) * 7.85 * Qg;  //Qft为通长顶筋间距200费用
-                double Qff200 = ((1000 / Ass[3][3]) * (Math.Pow((Ass[3][2] / 1000), 2) * Math.PI / 4) * (LffA / 1000) + (1000 / Ass[4][3]) * (Math.Pow((Ass[4][2] / 1000), 2) * Math.PI / 4) * (LffB / 1000)) * 7.85 * Qg;        //Qff为附加顶筋间距200费用
-
-                double Qz = ((1000 / Ass[6][1]) * (Math.Pow((Ass[6][0] / 1000), 2) * Math.PI / 4) * (Lz1 / 1000) + (1000 / Ass[7][1]) * (Math.Pow((Ass[7][0] / 1000), 2) * Math.PI / 4) * (Lz2 / 1000)) * 7.85 * Qg;              //Qz为底筋费用
-                double Qs = (1 * 2 * Ashui[0][4] * H[0] + 1 * 2 * Ashui[1][4] * H[1]) * 7.85 * Qg * Math.Pow(10, -9);//水平筋的费用
-                double Qt = ((1 * h[0] / 1000) * (H[0] / 1000) + (1 * h[1] / 1000) * (H[1] / 1000)) * Qh;//Qt为混凝土费用
-                double QF150 = Qft150 + Qff150;//QF150为间距150所有钢筋的费用
-                double QF200 = Qft200 + Qff200;//QF200为间距200所有钢筋的费用
-                double[] Q = { QF150, QF200, Qz, Qt, Qs, QF150 + Qz + Qt + Qs, QF200 + Qz + Qt + Qs };
-                //150顶筋费用 200顶筋费用 底筋费用 砼费用 水平筋费用 间距150总费用 间距200总费用
+                double[] Q = new double[] { QF150, QF200, Qz150, Qz200, Qs, Qt, QF150 + Qz150 + Qs + Qt, QF200 + Qz200 + Qs + Qt };
                 return Q;
             }
             else
@@ -216,7 +168,7 @@ namespace Dr.RetainingWall
                     if (i == 0)
                     {
                         arrLft150[i] = H[i] + arrLff[1] + Math.Max(35 * Math.Max(Ass[0][0], Ass[2][0]), 500);
-                        arrLft200[i] = H[i] + arrLff[1] + Math.Max(35 * Math.Max(Ass[5][0], Ass[7][0]), 500);
+                        arrLft200[i] = H[i] + arrLff[1] + Math.Max(35 * Math.Max(Ass[n+1][0], Ass[n+3][0]), 500);
                     }
                     else if (i == n - 1)
                     {
